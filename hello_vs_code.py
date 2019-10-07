8924bc5e571dc0d210e1d2941f9456970284face
Lukas D Hange <lhange42@ccs.local>
2019-10-07 12:31:26 -0400

    Commented on hello_vs_code

--- hello_vs_code.py
+++ hello_vs_code.py
@@ -1,8 +1,12 @@
-import board
+#Lukas Hange
+#This was the introduction to VS Code and simply printin something in the serial monitor
+#9/25/19
+
+import board #pylint: disable-msg=import-error
 import time
 
 
 
-while true
-    print(Mcdelivery)
+while True:
+    print("Mcdelivery")# prints Mcdelivery
     time.sleep(1)
