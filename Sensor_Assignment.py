60e2f85e367be362003a682fb83e1f1208941dc4
Lukas D Hange <lhange42@ccs.local>
2019-10-09 11:34:12 -0400

    commented on code

--- Sensor_Assignment.py
+++ Sensor_Assignment.py
@@ -2,14 +2,14 @@
 #9/11
 #Get sensor read distance and change color of inboard Led accordingly.
 
-import board
-import neopixel
+import board#pylint: disable-msg=import-error
+import neopixel#pylint: disable-msg=import-error
 import math
 import time
-import digitalio
-import adafruit_bus_device
-import adafruit_hcsr04
-import simpleio
+import digitalio#pylint: disable-msg=import-error
+import adafruit_bus_device#pylint: disable-msg=import-error
+import adafruit_hcsr04#pylint: disable-msg=import-error
+import simpleio#pylint: disable-msg=import-error
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D11, echo_pin=board.D12)
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness = .1)
@@ -26,14 +26,14 @@ while True:
    while True:
 
     try:
-        print((sonar.distance,))
-        if sonar.distance <= 20:
+        print((sonar.distance,))# prints the distance
+        if sonar.distance <= 20:# when distance is less then 20 it starts red and goes to blue
 
             r = simpleio.map_range(sonar.distance, 0,20,255,0)
             b = simpleio.map_range(sonar.distance, 5,20,0,255)
             g = simpleio.map_range(sonar.distance, 20,35,0,255)
 
-        else:
+        else: # when its not less than or equal to 20 it will go from blue to green
             r = simpleio.map_range(sonar.distance, 0,20,255,0)
             b = simpleio.map_range(sonar.distance, 35,20,0,255)
             g = simpleio.map_range(sonar.distance, 20,35,0,255)
--- Servo_Capacitive_Touch_Assignment.py
+++ Servo_Capacitive_Touch_Assignment.py
@@ -1,41 +1,44 @@
+#Lukas Hange
 #
-#
-#
+#Making a wire turn one way or the other depending on which wire is touched
 
 import time
-import board
-import pulseio
+import board#pylint: disable-msg=import-error
+import pulseio#pylint: disable-msg=import-error
 import math
-import adafruit_motor
-import neopixel
-import digitalio
-import analogio
-import touchio
-from adafruit_motor import servo
+import adafruit_motor#pylint: disable-msg=import-error
+import neopixel#pylint: disable-msg=import-error
+import digitalio#pylint: disable-msg=import-error
+import analogio#pylint: disable-msg=import-error
+import touchio#pylint: disable-msg=import-error
+from adafruit_motor import servo#pylint: disable-msg=import-error
+
+Servo_angle = 0
 
 
 pwm = pulseio.PWMOut(board.D9, duty_cycle=2 ** 15, frequency=50)
 
-
-Servo_Angle = 0
-
-touch_A1 = touchio.TouchIn(board.A1)
-touch_A2 = touchio.TouchIn(board.A2)
+touch_A1 = touchio.TouchIn(board.A1)# assigning touch_a1 to pin A1
+touch_A2 = touchio.TouchIn(board.A2)# assigning touch_a2 to pin A2
 
 my_servo = servo.Servo(pwm)
 
 while True:
 
-   if touch_A1.value and Servo_Angle < 180:
+    if touch_A1.value and Servo_angle < 180:# when the first wire is touched and the servo degree<180 make servo angle increase by 5
         print('A1 Touched')
-        Servo_Angle += 1
-        my_servo.angle = Servo_Angle
+        Servo_angle += 5
+        my_servo.angle = Servo_angle
+        print(Servo_angle)
 
-   if touch_A2.value and Servo_Angle > 0:
+
+
+
+
+    if touch_A2.value and Servo_angle > 0:# when the second wire is touched and the servo degree>0 make servo angle is decreased by 5
         print('A2 Touched')
-        Servo_Angle -= 1
-        my_servo.angle = Servo_Angle
+        Servo_angle -= 5
+        my_servo.angle = Servo_angle
+        print(Servo_angle)
 
-
-
-time.sleep(0.001)
+    time.sleep(.01)
